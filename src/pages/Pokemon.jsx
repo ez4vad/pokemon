543f67a9712dc847cdd02cd1d7695dfe6d0dcb87
[vadym] <[itsvadikk@gmail.com]>
2024-10-14 18:47:49 +0200

    added details

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,17 +1,20 @@
-import { useLoaderData, } from "react-router-dom";
+import { Link, useLoaderData, } from "react-router-dom";
 import { pokemonapiService } from "../services/PokemonApiService";
 
 export const loader = async ({ params }) => {
-    const pokemon = await pokemonapiService.getPokemonByName(params.pokemonId);
+    const pokemon = await pokemonapiService.getPokemonByName(params.pokemonSlug);
     return { pokemon };
 }
 
 export const Pokemon = () => {
     const { pokemon } = useLoaderData();
     return (
+    <div className="is-flex is-justify-content-center">
     <div className="section">
         <h1 className="title is-1 is-capitalized">{ pokemon.name }</h1>
-        <img className="image-is-square" src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
+        <img className="image is-square" src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
+    <Link to={`/${pokemon.name}/details`} className="box button is-warning">Show more</Link>
+    </div>
     </div>
     );
 }
